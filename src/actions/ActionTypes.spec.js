be7d39933d2d9e0c2027bdd7d27c399a7ec128ed
Alberto Giorgi <gioralb@gmail.com>
2018-06-07 16:56:06 +0100

    Use ES6 notation for action types tests

--- src/actions/ActionTypes.spec.js
+++ src/actions/ActionTypes.spec.js
@@ -1,15 +1,15 @@
 import * as types from "../../src/actions/ActionTypes";
 
 describe("Action Types", () => {
-  test("Action types have all different values", () => {
+  test("All action types should have different values", () => {
     const values = Object.values(types);
     const valueSet = new Set(values);
     expect(values.length).toEqual(valueSet.size);
   });
 
-  test("Action keys and values match", () => {
-    for (let actionType of Object.keys(types)) {
+  test("Action keys and values should match", () => {
+    Object.keys(types).forEach(actionType => {
       expect(types[actionType]).toEqual(actionType);
-    }
+    });
   });
 });
