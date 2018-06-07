35b8e644521e6091db72c190fe928915d70244f2
Alberto Giorgi <gioralb@gmail.com>
2018-06-07 22:42:46 +0100

    Added tests for parse input function

--- src/api/input.spec.js
+++ src/api/input.spec.js
@@ -14,12 +14,39 @@ describe("Input Parser", () => {
   });
 
   it("Should not accept dirt or robot location beyond boundaries", () => {
-    const parsedFile = parseInput("10 10\n10 2\r\n1 0\n2 2\n2 3\nNNENWW");
+    let parsedFile = parseInput("10 10\n10 2\r\n1 0\n2 2\n2 3\nNNENWW");
+    expect(parsedFile.isInputValid).toEqual(false);
+
+    parsedFile = parseInput("10 10\n1 10\r\n1 0\n2 2\n2 3\nNNENWW");
+    expect(parsedFile.isInputValid).toEqual(false);
+
+    parsedFile = parseInput("10 10\n1 1\r\n10 0\n2 2\n2 3\nNNENWW");
+    expect(parsedFile.isInputValid).toEqual(false);
+
+    parsedFile = parseInput("10 10\n1 10\r\n1 10\n2 2\n2 3\nNNENWW");
     expect(parsedFile.isInputValid).toEqual(false);
   });
 
   it("Should not accept overlapping robot and/or dirt patches", () => {
-    const parsedFile = parseInput("10 10\n1 1\r\n1 1\n2 2\n2 3\nNNENWW");
+    let parsedFile = parseInput("10 10\n1 1\r\n1 1\n2 2\n2 3\nNNENWW");
+    expect(parsedFile.isInputValid).toEqual(false);
+
+    parsedFile = parseInput("10 10\n1 1\r\n2 2\n2 2\n4 4\nNNENWW");
+    expect(parsedFile.isInputValid).toEqual(false);
+  });
+
+  it("Should not accept empty directions line", () => {
+    const parsedFile = parseInput("10 10\n1 1\r\n1 1\n2 2\n2 3");
+    expect(parsedFile.isInputValid).toEqual(false);
+  });
+
+  it("Should not accept invalid characters in locations", () => {
+    const parsedFile = parseInput("10 10\n1.1 1\r\n2 3\n4 5\n6 7\nNNENWW");
+    expect(parsedFile.isInputValid).toEqual(false);
+  });
+
+  it("Should not accept invalid characters in directions", () => {
+    const parsedFile = parseInput("10 10\n1 1\r\n2 3\n4 5\n6 7\nANNENWW");
     expect(parsedFile.isInputValid).toEqual(false);
   });
 });
